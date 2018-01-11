2503053c559331857a4077e193447d3d6c190085
I. Auda <ivan.auda@hva.nl>
2018-01-11 16:43:53 +0100

    Project Created! **EXCITED**

---
+++ Assets/Scripts/UI/FadeInScreen.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInScreen : MonoBehaviour {
+
+    IEnumerator Start() {
+        yield return new WaitForSeconds(1f);
+    }
+}
